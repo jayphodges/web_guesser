aa6061a7937b379cc20b98d69a022ae6a48d0ab4
jayphodges <jayphodges@gmail.com>
2017-08-07 19:39:53 -0600

    I4 extension color background added

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,7 +2,39 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 
-number = rand(101)
+set :number, rand(0..100)
+
 get '/' do
-  erb :index, :locals => {:number => number}
+  guess = params["guess"].to_i
+  message = number_guesser(guess)
+  background = background(message)
+  erb :index, :locals => {:number => settings.number,
+                          :message => message,
+                          :background => background}
+end
+
+def number_guesser(guess)
+  if guess > (settings.number + 5)
+    "Way too high!"
+  elsif guess < (settings.number - 5)
+    "Way too low!"
+  elsif guess > settings.number
+    "Too high!"
+  elsif guess < settings.number
+    "Too low!"
+  else
+    "You got it right! You guessed the secret number #{settings.number}"
+  end
+end
+
+def background(message)
+  if message == "Way too High!" || message == "Way too low!"
+    "red"
+  elsif message == "Too high!" || message == "Too low!"
+    "#FFF4F3"
+  elsif message == "You got it right! You guessed the secret number #{settings.number}"
+    "#0FAD00"
+  else
+    "white"
+  end
 end
